12b423f90571914f17af1e03f36c85e1505c107a
Wooksang Ryu <today8934@gmail.com>
2022-06-06 14:37:52 +0900

    Thread-Safe with synchronized

--- src/main/java/com/example/gofdesignpattern/singleton/Settings.java
+++ src/main/java/com/example/gofdesignpattern/singleton/Settings.java
@@ -6,7 +6,13 @@ public class Settings {
 
     private Settings() {}
 
-    public static Settings getInstance() {
+    /*
+    가장 간단한 방법의 Thread-Safe 싱글톤.
+    synchronized 키워드를 이용하여 동시에 하나의 스레드만 해당 메소드에 접근할 수 있게 된다.
+    (하나의 스레드 접근 시 lock이 걸림)
+    하지만 동시에 하나의 스레드만 접근이 가능하므로 성능에 이슈가 있을 수 있음.
+     */
+    public static synchronized Settings getInstance() {
         if (instance == null) {
             instance = new Settings();
         }
