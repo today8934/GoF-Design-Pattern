bba71fa6c008885aa00bd5f55673eb31cb1372dc
Wooksang Ryu <today8934@gmail.com>
2022-06-06 14:58:14 +0900

    Reflection을 이용하여 싱글톤 깨기

--- src/main/java/com/example/gofdesignpattern/GofDesignPatternApplication.java
+++ src/main/java/com/example/gofdesignpattern/GofDesignPatternApplication.java
@@ -4,13 +4,25 @@ import com.example.gofdesignpattern.singleton.Settings;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 @SpringBootApplication
 public class GofDesignPatternApplication {
 
-    public static void main(String[] args) {
+    /*
+    Reflection을 이용하여 싱글톤 깨기
+    
+     */
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         Settings settings = Settings.getInstance();
         Settings settings1 = Settings.getInstance();
-        System.out.println(settings == settings1);
+
+        Constructor<Settings> constructor = Settings.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        Settings settings2 = constructor.newInstance();
+
+        System.out.println(settings2 == settings);
     }
 
 }
