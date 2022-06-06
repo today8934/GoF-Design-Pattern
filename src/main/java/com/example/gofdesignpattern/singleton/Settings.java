5d3f315a537fd49005eeafce245cb746b90614a8
Wooksang Ryu <today8934@gmail.com>
2022-06-06 14:46:43 +0900

    Double Checked Locking

--- src/main/java/com/example/gofdesignpattern/singleton/Settings.java
+++ src/main/java/com/example/gofdesignpattern/singleton/Settings.java
@@ -2,15 +2,27 @@ package com.example.gofdesignpattern.singleton;
 
 public class Settings {
 
-    private static final Settings INSTANCE = new Settings();
+    private static volatile Settings instance;
 
     private Settings() {}
 
     /*
-    이른 초기화(Eager Initialization)
-    미리 만들어 놓는다는 것 자체가 단점이 될 수 있다.(만들어놓고 안쓸 수 있음. 리소스 낭비)
+    double checked locking
+    메서드에 접근하는 모든 스레드에 대해 synchronized가 걸리는게 아니라
+    if문 안에서 동기화에 걸리기 때문에 메서드에 synchronized 키워드를 붙이는 것보다 성능적으로 우위에 있을 수 있다.
+    (메서드에서부터 동기화에 걸리는것이 아니라 첫번째 if문에서 instance가 null일 경우 바로 instance를 리턴해버리기때문에)
+    인스턴스를 필요한 시점에 만들 수 있음.
+    jdk 1.5부터 동작한다.
      */
     public static Settings getInstance() {
-        return INSTANCE;
+        if (instance == null) {
+            synchronized (Settings.class) {
+                if (instance == null) {
+                    instance = new Settings();
+                }
+            }
+        }
+
+        return instance;
     }
 }
